8c2c08cdb8a799f24cd0a8bcf83a21889608f181
Carlos Marin <carlosmarinphs@gmail.com>
2023-12-10 12:35:27 -0500

    Finished JS

--- scripts/newUser.js
+++ scripts/newUser.js
@@ -9,7 +9,22 @@ window.onload = () => {
       name: name,
       username: username,
     };
+    
+    // console.log(user)
 
-    console.log(user);
+    fetch(`http://localhost:8083/api/users`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("User Registered:", data);
+        alert("User registered successfully!");
+
+        window.location.href = "index.html";
+      });
   };
 };
